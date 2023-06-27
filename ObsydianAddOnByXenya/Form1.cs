7ad26b94670de4b2b89e7bf482169d4ca596dff2
Xennoxx <nicknifffe@gmail.com>
2023-06-27 10:53:42 +0200

    Wrote a little bit explanation

--- ObsydianAddOnByXenya/Form1.cs
+++ ObsydianAddOnByXenya/Form1.cs
@@ -4,6 +4,12 @@ namespace ObsydianAddOnByXenya
     {
         public Form1()
         {
+            //"Form1" Should have 4 buttopns (open foulder, syncGit, readme, settings)
+            //Class settings (additional setting eventualy a snyc via. google should be possible too..)
+            //Class Git (void Pushing method)
+            //Class Obsidian(connecting obisdian and the folder with the program)
+            //Class readme (to open the readme)
+            //Class execution (the execution of the programm should be separated in the class)
             InitializeComponent();
         }
 
